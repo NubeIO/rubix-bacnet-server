b0545b1f2d2ee90d3c31d89fbe707a020395edd9
RaiBnod <binodrayee@gmail.com>
2020-11-12 19:14:08 +0545

    Update BACnet values as per the RESTful entries (#16)

--- src/bacnet_server/feedbacks/analog_output.py
+++ src/bacnet_server/feedbacks/analog_output.py
@@ -13,11 +13,10 @@ class AnalogOutputFeedbackObject(AnalogOutputCmdObject):
         self._property_monitors["presentValue"].append(self.check_feedback)
 
     def check_feedback(self, old_value, new_value):
-
         priority_array = self._dict_contents().get('priorityArray')
         serialized_priority_array = serialize_priority_array(priority_array)
-        PriorityArrayModel.filter_by_point_uuid(self.objectName).update(serialized_priority_array)
-        update_point_store(self.objectName, new_value)
+        PriorityArrayModel.filter_by_point_uuid(self.profileName).update(serialized_priority_array)
+        update_point_store(self.profileName, new_value)
         object_identifier = create_object_identifier(self.objectIdentifier[0], self.objectIdentifier[1])
         present_value = self.presentValue
         if isinstance(present_value, Real):
