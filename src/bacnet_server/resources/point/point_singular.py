53ea9403d08d36ca708264efe111aaf3f754027a
RaiBnod <binodrayee@gmail.com>
2020-11-13 11:57:43 +0545

    Change PUT request to PATCH

--- src/bacnet_server/resources/point/point_singular.py
+++ src/bacnet_server/resources/point/point_singular.py
@@ -1,6 +1,6 @@
 import copy
 
-from flask_restful import abort, marshal_with
+from flask_restful import abort, marshal_with, reqparse
 
 from src.bacnet_server.bac_server import BACServer
 from src.bacnet_server.helpers.helper_mqtt import publish_mqtt_value
@@ -11,10 +11,18 @@ from src.bacnet_server.resources.point.point_base import BACnetPointBase
 
 
 class BACnetPointSingular(BACnetPointBase):
-    """
-    It returns point with point_store object value, which has the current values of point_store for that particular
-    point with last not null value and value_array
-    """
+    parser_patch = reqparse.RequestParser()
+    parser_patch.add_argument('object_type', type=str, required=False)
+    parser_patch.add_argument('object_name', type=str, required=False)
+    parser_patch.add_argument('address', type=int, required=False)
+    parser_patch.add_argument("priority_array_write", type=dict, required=False)
+    parser_patch.add_argument('relinquish_default', type=float, required=False)
+    parser_patch.add_argument('units', type=str, required=False)
+    parser_patch.add_argument('description', type=str, required=False)
+    parser_patch.add_argument('enable', type=bool, required=False)
+    parser_patch.add_argument('fault', type=bool, required=False)
+    parser_patch.add_argument('data_round', type=int, required=False)
+    parser_patch.add_argument('data_offset', type=float, required=False)
 
     @marshal_with(point_fields)
     def get(self, uuid):
@@ -24,15 +32,20 @@ class BACnetPointSingular(BACnetPointBase):
         return point
 
     @marshal_with(point_fields)
-    def put(self, uuid):
-        data = BACnetPointSingular.parser.parse_args()
+    def patch(self, uuid):
+        data = BACnetPointSingular.parser_patch.parse_args()
         point = copy.deepcopy(BACnetPointModel.find_by_uuid(uuid))
         if point is None:
-            return self.add_point(data, uuid)
+            abort(404, message=f"Does not exist {uuid}")
         try:
             priority_array_write = data.pop('priority_array_write')
-            BACnetPointModel.filter_by_uuid(uuid).update(data)
-            PriorityArrayModel.filter_by_point_uuid(uuid).update(priority_array_write)
+            non_none_data = {}
+            for key in data.keys():
+                if data[key] is not None:
+                    non_none_data[key] = data[key]
+            BACnetPointModel.filter_by_uuid(uuid).update(non_none_data)
+            if priority_array_write:
+                PriorityArrayModel.filter_by_point_uuid(uuid).update(priority_array_write)
             BACServer.get_instance().remove_point(point)
             point_return = BACnetPointModel.find_by_uuid(uuid)
             [object_identifier, present_value] = BACServer.get_instance().add_point(point_return)
