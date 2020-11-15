7b9c9efaade400b79cac62aa7d57f8e7733f85e4
Aidan P <aidan@enviroservices.com.au>
2020-11-15 19:45:56 +1100

    Update install.bash

--- install.bash
+++ install.bash
@@ -7,7 +7,7 @@ virtualenv -p python3 venv
 source venv/bin/activate
 pip3 install -r requirements.txt
 deactivate
-sudo cp systemd/nubeio-bac-rest.service /etc/systemd/system/
+sudo cp systemd/nubeio-bacnet-server.service /etc/systemd/system/
 sudo systemctl daemon-reload
-sudo systemctl enable nubeio-bac-rest.service
-sudo systemctl start nubeio-bac-rest.service
+sudo systemctl enable nubeio-bacnet-server.service
+sudo systemctl start nubeio-bacnet-server.service
