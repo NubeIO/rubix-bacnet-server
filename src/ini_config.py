d66851464c7a36600a9eee2d15a9552f3f5c0a91
NubeDev <aidan@envrioservices.com.au>
2020-11-19 09:36:20 +1100

    fix

--- src/ini_config.py
+++ src/ini_config.py
@@ -1,12 +1,13 @@
 """Load configuration from .ini file."""
 import configparser
-
 import os
-
+temp_path = os.path.dirname(os.path.abspath(__file__))
+part_config = os.path.join(temp_path, "../settings/config.ini")
 config = configparser.ConfigParser()
 if os.environ.get("data_dir") is None:
     filename = '/settings/config.ini'
 else:
     filename = f'{os.environ.get("data_dir")}/config.ini'
 
-config.read(filename)
+config.read(part_config)
+
