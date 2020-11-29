ef00111d7bb2e98deccf6e2fc597c516bb00201b
RaiBnod <binodrayee@gmail.com>
2020-11-29 20:54:19 +0545

    Make /api/ping for showing all status & flags

--- src/bacnet_server/resources/server/server_status.py
+++
@@ -1,8 +0,0 @@
-from flask_restful import Resource
-
-from src.bacnet_server.bac_server import BACServer
-
-
-class BACnetServerStatus(Resource):
-    def get(self):
-        return {'running': BACServer.get_instance().is_running()}, 200
