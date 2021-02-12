1d25c0e00c83df74870eab3fc521b315be1df8fc
RaiBnod <binodrayee@gmail.com>
2021-02-12 19:12:25 +0545

    Remove old codes

--- src/routes.py
+++ src/routes.py
@@ -1,8 +1,6 @@
 from flask import Blueprint
 from flask_restful import Api
 
-from src.bacnet_master.resources.device import Device, DeviceList, DevicePoints, DevicePoint, PointWritePresentValue
-from src.bacnet_master.resources.network import Network, NetworkList, NetworksIds
 from src.bacnet_server.resources.mapping.mapping import BPGPMappingResourceList, GBPMappingResourceByGenericPointUUID, \
     GBPMappingResourceByBACnetPointUUID
 from src.bacnet_server.resources.point.point_name import BACnetPointName
@@ -32,22 +30,6 @@ api_bp_gp_mapping.add_resource(GBPMappingResourceByGenericPointUUID, '/generic/<
 bp_bp_gp_sync = Blueprint('bp_gp_sync', __name__, url_prefix='/api/bp_gp/sync')
 api_bp_gp_sync = Api(bp_bp_gp_sync)
 api_bp_gp_sync.add_resource(BPGPSync, '')
-
-bp_bacnet_master = Blueprint('bacnet_master', __name__, url_prefix='/api/bac/master')
-api_bacnet_master = Api(bp_bacnet_master)
-
-api_bacnet_master.add_resource(Device, '/dev/<string:uuid>')
-api_bacnet_master.add_resource(Network, '/network/<string:uuid>')
-api_bacnet_master.add_resource(DeviceList, '/devices')
-api_bacnet_master.add_resource(DevicePoints, '/points/objects/<string:dev_uuid>')
-# get a point /dev_uuid/analogInput/1/85
-api_bacnet_master.add_resource(DevicePoint,
-                               '/point/read/<string:dev_uuid>/<string:obj>/<string:obj_instance>/<string:prop>')
-api_bacnet_master.add_resource(NetworkList, '/networks')
-api_bacnet_master.add_resource(NetworksIds, '/networks/ids')
-
-api_bacnet_master.add_resource(PointWritePresentValue,
-                               '/point/write/<string:dev_uuid>/<string:obj>/<string:obj_instance>/<string:value>')
 
 bp_system = Blueprint('system', __name__, url_prefix='/api/system')
 api_system = Api(bp_system)
