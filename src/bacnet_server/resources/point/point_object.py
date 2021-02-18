39e79604fbd0c696862e76a1aaced1c99a26f05c
RaiBnod <binodrayee@gmail.com>
2021-02-18 13:20:07 +0545

    Singular points base creation for improvements

--- src/bacnet_server/resources/point/point_object.py
+++ src/bacnet_server/resources/point/point_object.py
@@ -1,57 +1,9 @@
-import copy
-
-from flask_restful import marshal_with, abort, reqparse
-from flask_restful.reqparse import request
-from mrb.validator import is_bridge
-
-from src.bacnet_server import BACServer
 from src.bacnet_server.models.model_point import BACnetPointModel
-from src.bacnet_server.models.model_priority_array import PriorityArrayModel
-from src.bacnet_server.resources.model_fields import point_fields
-from src.bacnet_server.resources.point.point_base import BACnetPointBase
+from src.bacnet_server.resources.point.point_base_singular import BACnetPointBaseSingular
 
 
-class BACnetPointObject(BACnetPointBase):
-    parser_patch = reqparse.RequestParser()
-    parser_patch.add_argument('object_name', type=str, required=False)
-    parser_patch.add_argument('relinquish_default', type=float, required=False)
-    parser_patch.add_argument("priority_array_write", type=dict, required=False)
-    parser_patch.add_argument('event_state', type=str, required=False)
-    parser_patch.add_argument('units', type=str, required=False)
-    parser_patch.add_argument('description', type=str, required=False)
-    parser_patch.add_argument('enable', type=bool, required=False)
-    parser_patch.add_argument('fault', type=bool, required=False)
-    parser_patch.add_argument('data_round', type=int, required=False)
-    parser_patch.add_argument('data_offset', type=float, required=False)
+class BACnetPointObject(BACnetPointBaseSingular):
 
     @classmethod
-    @marshal_with(point_fields)
-    def get(cls, object_type, address):
-        point = BACnetPointModel.find_by_object_id(object_type, address)
-        if not point:
-            abort(404, message='BACnet Point is not found')
-        return point
-
-    @classmethod
-    @marshal_with(point_fields)
-    def patch(cls, object_type, address):
-        data = BACnetPointObject.parser_patch.parse_args()
-        point = copy.deepcopy(BACnetPointModel.find_by_object_id(object_type, address))
-        cls.abort_if_bacnet_is_not_running()
-        if point is None:
-            abort(404, message=f"Does not exist {object_type}-{address}")
-        try:
-            priority_array_write = data.pop('priority_array_write')
-            non_none_data = {}
-            for key in data.keys():
-                if data[key] is not None:
-                    non_none_data[key] = data[key]
-            if priority_array_write:
-                PriorityArrayModel.filter_by_point_uuid(point.uuid).update(priority_array_write)
-            BACnetPointModel.filter_by_uuid(point.uuid).update(non_none_data)
-            BACServer().remove_point(point)
-            point_return = BACnetPointModel.find_by_uuid(point.uuid)
-            BACServer().add_point(point_return, not is_bridge(request.args))
-            return point_return
-        except Exception as e:
-            abort(500, message=str(e))
+    def get_point(cls, **kwargs) -> BACnetPointModel:
+        return BACnetPointModel.find_by_object_id(kwargs.get('object_type'), kwargs.get('address'))
--- src/bacnet_server/resources/point/point_singular.py
+++ src/bacnet_server/resources/point/point_singular.py
@@ -1,67 +1,9 @@
-import copy
-
-from flask_restful import abort, marshal_with, reqparse
-from flask_restful.reqparse import request
-from mrb.validator import is_bridge
-
-from src.bacnet_server import BACServer
 from src.bacnet_server.models.model_point import BACnetPointModel
-from src.bacnet_server.models.model_priority_array import PriorityArrayModel
-from src.bacnet_server.resources.model_fields import point_fields
-from src.bacnet_server.resources.point.point_base import BACnetPointBase
+from src.bacnet_server.resources.point.point_base_singular import BACnetPointBaseSingular
 
 
-class BACnetPointSingular(BACnetPointBase):
-    parser_patch = reqparse.RequestParser()
-    parser_patch.add_argument('object_type', type=str, required=False)
-    parser_patch.add_argument('object_name', type=str, required=False)
-    parser_patch.add_argument('address', type=int, required=False)
-    parser_patch.add_argument('relinquish_default', type=float, required=False)
-    parser_patch.add_argument("priority_array_write", type=dict, required=False)
-    parser_patch.add_argument('event_state', type=str, required=False)
-    parser_patch.add_argument('units', type=str, required=False)
-    parser_patch.add_argument('description', type=str, required=False)
-    parser_patch.add_argument('enable', type=bool, required=False)
-    parser_patch.add_argument('fault', type=bool, required=False)
-    parser_patch.add_argument('data_round', type=int, required=False)
-    parser_patch.add_argument('data_offset', type=float, required=False)
+class BACnetPointSingular(BACnetPointBaseSingular):
 
     @classmethod
-    @marshal_with(point_fields)
-    def get(cls, uuid):
-        point = BACnetPointModel.find_by_uuid(uuid)
-        if not point:
-            abort(404, message='BACnet Point is not found')
-        return point
-
-    @classmethod
-    @marshal_with(point_fields)
-    def patch(cls, uuid):
-        data = BACnetPointSingular.parser_patch.parse_args()
-        point = copy.deepcopy(BACnetPointModel.find_by_uuid(uuid))
-        cls.abort_if_bacnet_is_not_running()
-        if point is None:
-            abort(404, message=f"Does not exist {uuid}")
-        try:
-            priority_array_write = data.pop('priority_array_write')
-            non_none_data = {}
-            for key in data.keys():
-                if data[key] is not None:
-                    non_none_data[key] = data[key]
-            BACnetPointModel.filter_by_uuid(uuid).update(non_none_data)
-            if priority_array_write:
-                PriorityArrayModel.filter_by_point_uuid(uuid).update(priority_array_write)
-            BACServer().remove_point(point)
-            point_return = BACnetPointModel.find_by_uuid(uuid)
-            BACServer().add_point(point_return, not is_bridge(request.args))
-            return point_return
-        except Exception as e:
-            abort(500, message=str(e))
-
-    @classmethod
-    def delete(cls, uuid):
-        point = BACnetPointModel.find_by_uuid(uuid)
-        if point:
-            BACServer().remove_point(point)
-            point.delete_from_db()
-        return '', 204
+    def get_point(cls, **kwargs) -> BACnetPointModel:
+        return BACnetPointModel.find_by_uuid(kwargs.get('uuid'))
