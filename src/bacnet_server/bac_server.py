3fdceb4596d88c3e7a5e989ca159a97344edaba1
RaiBnod <binodrayee@gmail.com>
2021-03-03 15:04:41 +0545

    Stackoverflow issue fix: infinite recursion is removed out

--- src/bacnet_server/bac_server.py
+++ src/bacnet_server/bac_server.py
@@ -1,9 +1,11 @@
 import copy
 import logging
 import time
+from typing import Union, Dict
 
 import BAC0
 from bacpypes.basetypes import EngineeringUnits
+from bacpypes.local.object import Commandable
 
 from src import BACnetSetting
 from src.bacnet_server.feedbacks.analog_output import AnalogOutputFeedbackObject
@@ -21,15 +23,15 @@ logger = logging.getLogger(__name__)
 class BACServer(metaclass=Singleton):
 
     def __init__(self):
-        self.__config = None
-        self.__bacnet = None
-        self.__bacnet_server = None
-        self.__new_bacnet_server = None
-        self.__registry = {}
-        self.__sync_status = False
+        self.__config: Union[BACnetSetting, None] = None
+        self.__bacnet: Union[BAC0, None] = None
+        self.__bacnet_server: Union[BACnetServerModel, None] = None
+        self.__registry: Dict[str, Commandable] = {}
+        self.__sync_status: bool = False
+        self.__running: bool = False
 
     @property
-    def config(self) -> BACnetSetting:
+    def config(self) -> Union[BACnetSetting, None]:
         return self.__config
 
     def status(self) -> bool:
@@ -41,41 +43,30 @@ class BACServer(metaclass=Singleton):
         self.loop_forever()
 
     def loop_forever(self):
-        try:
-            self.connect(self.__bacnet_server)
-        except Exception as e:
-            logger.error(e)
-            logger.warning("BACnet is not connected, waiting for BACnet server connection...")
-            self.check_to_restart()
-            time.sleep(self.config.attempt_reconnect_secs)
-            self.loop_forever()
-
-        mqtt_client = MqttClient()
-        if mqtt_client.config.enabled and mqtt_client.config.publish_value:
-            while not mqtt_client.status():
-                self.check_to_restart()
-                logger.warning("MQTT is not connected, waiting for MQTT connection successful...")
+        while True:
+            try:
+                if not self.__running:
+                    self.connect(self.__bacnet_server)
+                    mqtt_client = MqttClient()
+                    if mqtt_client.config.enabled and mqtt_client.config.publish_value:
+                        while not mqtt_client.status():
+                            logger.warning("MQTT is not connected, waiting for MQTT connection successful...")
+                            time.sleep(self.config.attempt_reconnect_secs)
+                    self.sync_stack()
+                    self.__running = True
+                time.sleep(2)
+            except Exception as e:
+                logger.error(e)
+                logger.warning("BACnet is not connected, waiting for BACnet server connection...")
                 time.sleep(self.config.attempt_reconnect_secs)
-        self.sync_stack()
 
-        # keep listening changes
-        while not (self.__new_bacnet_server and self.__new_bacnet_server != self.__bacnet_server):
-            time.sleep(2)
-        self.__bacnet_server = self.__new_bacnet_server
-        self.loop_forever()
-
-    def check_to_restart(self):
-        if self.__new_bacnet_server and self.__new_bacnet_server != self.__bacnet_server:
-            self.__bacnet_server = self.__new_bacnet_server
-            self.loop_forever()
-
-    def restart_bac(self, new_bacnet_server):
+    def restart_bac(self, bacnet_server):
         if self.__bacnet:
             self.__bacnet.disconnect()  # on macOS it's not working
             time.sleep(1)  # as per their testing we need to sleep to make sure all sockets got closed
 
         self.__reset_variable()
-        self.__new_bacnet_server = new_bacnet_server
+        self.__bacnet_server = bacnet_server
 
     def sync_stack(self):
         for point in BACnetPointModel.query.filter_by(object_type=PointType.analogOutput):
@@ -93,6 +84,7 @@ class BACServer(metaclass=Singleton):
 
     def __reset_variable(self):
         self.__bacnet = None
+        self.__running = False
         self.__registry = {}
 
     def add_point(self, point, sync: bool = True):
