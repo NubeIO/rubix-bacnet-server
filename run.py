6e8d347e9a8464a7f21231de9808471370f6ea33
Enju Thulung <enjuthulung@gmail.com>
2021-03-03 23:42:35 +0545

    Separate data and config files (#100)

--- run.py
+++ run.py
@@ -17,19 +17,25 @@ def number_of_workers():
 
 @click.command(context_settings=CLI_CTX_SETTINGS)
 @click.option('-p', '--port', type=int, default=AppSetting.PORT, show_default=True, help='Port')
+@click.option('-g', '--global-dir', type=click.Path(), help='Global dir',
+              default=lambda: os.environ.get(AppSetting.GLOBAL_DIR_ENV))
 @click.option('-d', '--data-dir', type=click.Path(), help='Application data dir',
-              default=lambda: os.environ.get(AppSetting.DATA_DIR_ENV, AppSetting.default_data_dir))
+              default=lambda: os.environ.get(AppSetting.DATA_DIR_ENV))
+@click.option('-c', '--config-dir', type=click.Path(), help='Application config dir',
+              default=lambda: os.environ.get(AppSetting.CONFIG_DIR_ENV))
 @click.option('-i', '--identifier', type=str, default=AppSetting.default_identifier, show_default=True,
               help='Identifier')
 @click.option('--prod', is_flag=True, help='Production mode')
 @click.option('-s', '--setting-file', help='Rubix BACnet: setting json file', default=AppSetting.default_setting_file)
 @click.option('-l', '--logging-conf', help='Rubix BACnet: logging config file')
 @click.option('--workers', type=int, help='Gunicorn: The number of worker processes for handling requests.')
-@click.option('-c', '--gunicorn-config', help='Gunicorn: config file(gunicorn.conf.py)')
+@click.option('--gunicorn-config', help='Gunicorn: config file(gunicorn.conf.py)')
 @click.option('--log-level', type=click.Choice(['FATAL', 'ERROR', 'WARN', 'INFO', 'DEBUG'], case_sensitive=False),
               show_default=True, help='Logging level')
-def cli(port, data_dir, identifier, prod, workers, setting_file, logging_conf, gunicorn_config, log_level):
-    setting = AppSetting(port=port, data_dir=data_dir, identifier=identifier, prod=prod).reload(setting_file)
+def cli(port, global_dir, data_dir, config_dir, identifier, prod, workers, setting_file, logging_conf, gunicorn_config,
+        log_level):
+    setting = AppSetting(port=port, global_dir=global_dir, data_dir=data_dir, config_dir=config_dir,
+                         identifier=identifier, prod=prod).reload(setting_file)
     options = {
         'bind': '%s:%s' % ('0.0.0.0', setting.port),
         # 'workers': workers if workers is not None else number_of_workers() if prod else 1,
