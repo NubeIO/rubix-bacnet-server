4776cdb5d419ae2d52b111bcac972c129b9ebc7f
NubeDev <aidan@envrioservices.com.au>
2021-04-14 14:33:23 +1000

    got bacnet master working

--- src/routes.py
+++ src/routes.py
@@ -11,7 +11,7 @@ from src.bacnet_server.resources.point.point_singular import BACnetPointSingular
     BACnetPointSingularByName, BACnetPointSingularByObject
 from src.bacnet_server.resources.point.point_sync import BPToGPSync
 from src.bacnet_server.resources.server.server import BACnetServer
-from src.system.resources.ping import Ping
+from src.system.resources.ping import Ping, Ping2
 
 bp_bacnet_server = Blueprint('bacnet_server', __name__, url_prefix='/api/bacnet')
 api_bacnet_server = Api(bp_bacnet_server)
@@ -25,26 +25,23 @@ api_bacnet_server.add_resource(BACnetPointSingularByName, '/points/name/<string:
 # BACnet points <> Generic points mappings
 bp_mapping_bp_gp = Blueprint('mappings_bp_gp', __name__, url_prefix='/api/mappings/bp_gp')
 api_mapping_bp_gp = Api(bp_mapping_bp_gp)
+
 api_mapping_bp_gp.add_resource(BPGPMappingResourceList, '')
 api_mapping_bp_gp.add_resource(BPGPMappingResourceByUUID, '/uuid/<string:uuid>')
 api_mapping_bp_gp.add_resource(GBPMappingResourceByBACnetPointUUID, '/bacnet/<string:uuid>')
 api_mapping_bp_gp.add_resource(GBPMappingResourceByGenericPointUUID, '/generic/<string:uuid>')
 
-
-bp_bacnet_master = Blueprint('bacnet_master', __name__, url_prefix='/api/bac/master')
-api_bacnet_master = Api(bp_bacnet_master)
-api_bacnet_master.add_resource(Device, '/dev/<string:uuid>')
-api_bacnet_master.add_resource(Network, '/network/<string:uuid>')
-api_bacnet_master.add_resource(DeviceList, '/devices')
-api_bacnet_master.add_resource(DevicePoints, '/points/objects/<string:dev_uuid>')
+api_bacnet_server.add_resource(Device, '/master/dev/<string:uuid>')
+api_bacnet_server.add_resource(Network, '/master/network/<string:uuid>')
+api_bacnet_server.add_resource(DeviceList, '/master/devices')
+api_bacnet_server.add_resource(DevicePoints, '/master/points/objects/<string:dev_uuid>')
 # get a point /dev_uuid/analogInput/1/85
-api_bacnet_master.add_resource(DevicePoint,
-                               '/point/read/<string:dev_uuid>/<string:obj>/<string:obj_instance>/<string:prop>')
-api_bacnet_master.add_resource(NetworkList, '/networks')
-api_bacnet_master.add_resource(NetworksIds, '/networks/ids')
+api_bacnet_server.add_resource(DevicePoint,
+                               '/master/point/read/<string:dev_uuid>/<string:obj>/<string:obj_instance>/<string:prop>')
+api_bacnet_server.add_resource(NetworkList, '/master/networks')
+api_bacnet_server.add_resource(NetworksIds, '/master/networks/ids')
 # api_bacnet_master.add_resource(PointWritePresentValue,
 #                                '/point/write/<string:dev_uuid>/<string:obj>/<string:obj_instance>/<string:value>')
-
 
 
 bp_sync = Blueprint('sync_bp_gp', __name__, url_prefix='/api/sync')
@@ -54,3 +51,4 @@ api_sync.add_resource(BPToGPSync, '/bp_to_gp')
 bp_system = Blueprint('system', __name__, url_prefix='/api/system')
 api_system = Api(bp_system)
 api_system.add_resource(Ping, '/ping')
+api_system.add_resource(NetworkList, '/ping2')
--- src/system/resources/ping.py
+++ src/system/resources/ping.py
@@ -51,3 +51,35 @@ class Ping(RubixResource):
                 'status': MqttRestBridge.status()
             }
         }
+
+class Ping2(RubixResource):
+    @classmethod
+    def get(cls):
+        up_time = get_up_time()
+        up_min = up_time / 60
+        up_min = "{:.2f}".format(up_min)
+        up_hour = up_time / 3600
+        up_hour = "{:.2f}".format(up_hour)
+        from src import AppSetting
+        setting: AppSetting = current_app.config[AppSetting.FLASK_KEY]
+        deployment_mode = 'production' if setting.prod else 'development'
+
+        return {
+            'version': get_version(),
+            'up_time_date': up_time_date,
+            'up_min': up_min,
+            'up_hour': up_hour,
+            'deployment_mode': deployment_mode,
+            'bacnet': {
+                'enabled': setting.bacnet.enabled,
+                'status': BACServer().status(),
+            },
+            'mqtt': {
+                'enabled': setting.mqtt.enabled,
+                'status': MqttClient().status()
+            },
+            'mqtt_rest_bridge_listener': {
+                'enabled': setting.mqtt_rest_bridge_setting.enabled,
+                'status': MqttRestBridge.status()
+            }
+        }
