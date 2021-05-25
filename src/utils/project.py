e39043d07f78470752fec5266318cdab5454e15c
RaiBnod <binodrayee@gmail.com>
2021-05-25 13:52:32 +0545

    Add VERSION file

--- src/utils/project.py
+++ src/utils/project.py
@@ -1,9 +1,10 @@
-import toml
-
 from src.pyinstaller import resource_path
 
 
-def get_version():
-    details = toml.load(resource_path("pyproject.toml"))
-    version = details.get('tool', {}).get('poetry', {}).get('version')
-    return f'v{version}' if version else None
+def get_version() -> str:
+    try:
+        with open(resource_path('VERSION')) as version_file:
+            version = f'v{version_file.read().strip()}'
+    except FileNotFoundError:
+        version = 'v0.0.0'
+    return version
